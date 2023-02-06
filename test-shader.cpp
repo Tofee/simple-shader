280047d9f5f159072a20f49a6d777a9f8da039f4
Christophe Chapuis <chris.chapuis@gmail.com>
2023-02-06 21:40:54 +0100

    Add config dump and create pbuffer

--- test-shader.cpp
+++ test-shader.cpp
@@ -1,9 +1,11 @@
 #include <EGL/egl.h>
+#include <EGL/eglext.h>
 #include <GLES3/gl3.h>
 
 #include <stdlib.h>
 #include <fstream>
 #include <sstream>
+#include <iostream>
 #include <string>
 #include <cstdio>
 
@@ -51,12 +53,91 @@ public:
     eglTerminate(display);
   }
 
+  void dumpConfig(EGLDisplay display, EGLConfig *config) {
+     if(!config) return;
+     
+    EGLint value;
+     
+     eglGetConfigAttrib(display,*config,EGL_BUFFER_SIZE,&value);
+     std::cout<<"Buffer Size "<<value<<"\n";
+     eglGetConfigAttrib(display,*config,EGL_RED_SIZE,&value);
+     std::cout<<"Red Size "<<value<<"\n";
+     eglGetConfigAttrib(display,*config,EGL_GREEN_SIZE,&value);
+     std::cout<<"Green Size "<<value<<"\n";
+     eglGetConfigAttrib(display,*config,EGL_BLUE_SIZE,&value);
+     std::cout<<"Blue Size "<<value<<"\n";
+     eglGetConfigAttrib(display,*config,EGL_ALPHA_SIZE,&value);
+     std::cout<<"Alpha Size "<<value<<"\n";
+     eglGetConfigAttrib(display,*config,EGL_CONFIG_CAVEAT,&value);
+     switch(value)
+     {
+         case  EGL_NONE : std::cout<<"EGL_CONFIG_CAVEAT EGL_NONE\n"; break;
+         case  EGL_SLOW_CONFIG : std::cout<<"EGL_CONFIG_CAVEAT EGL_SLOW_CONFIG\n"; break;
+     }
+     eglGetConfigAttrib(display,*config,EGL_CONFIG_ID,&value);
+     std::cout<<"Config ID "<<value<<"\n";
+
+     // EGL_RENDERABLE_TYPE is a bit field
+     eglGetConfigAttrib(display,*config,EGL_RENDERABLE_TYPE,&value);
+     std::cout<<"Renderable Type: ";
+     if (( value & EGL_OPENGL_ES2_BIT ) == EGL_OPENGL_ES2_BIT ) std::cout<<"EGL_OPENGL_ES2_BIT ";
+     if (( value & EGL_OPENGL_ES3_BIT_KHR ) == EGL_OPENGL_ES3_BIT_KHR ) std::cout<<"EGL_OPENGL_ES3_BIT_KHR ";
+     std::cout<<"\n";
+     
+     eglGetConfigAttrib(display,*config,EGL_DEPTH_SIZE,&value);
+     std::cout<<"Depth size "<<value<<"\n";
+
+     eglGetConfigAttrib(display,*config,EGL_MAX_PBUFFER_WIDTH,&value);
+     std::cout<<"Max pbuffer width "<<value<<"\n";
+     eglGetConfigAttrib(display,*config,EGL_MAX_PBUFFER_HEIGHT,&value);
+     std::cout<<"Max pbuffer height "<<value<<"\n";
+     eglGetConfigAttrib(display,*config,EGL_MAX_PBUFFER_PIXELS,&value);
+     std::cout<<"Max pbuffer pixels "<<value<<"\n";
+     eglGetConfigAttrib(display,*config,EGL_NATIVE_RENDERABLE,&value);
+     std::cout<<"Native renderable "<<std::string(value ? "true" : "false")<<"\n";
+     eglGetConfigAttrib(display,*config,EGL_NATIVE_VISUAL_ID,&value);
+     std::cout<<"Native visual ID "<<value<<"\n";
+     eglGetConfigAttrib(display,*config,EGL_NATIVE_VISUAL_TYPE,&value);
+     std::cout<<"Native visual type "<<value<<"\n";
+     eglGetConfigAttrib(display,*config,EGL_SAMPLE_BUFFERS,&value);
+     std::cout<<"Sample Buffers "<<value<<"\n";
+     eglGetConfigAttrib(display,*config,EGL_SAMPLES,&value);
+     std::cout<<"Samples "<<value<<"\n";
+     eglGetConfigAttrib(display,*config,EGL_SURFACE_TYPE,&value);
+     std::cout<<"Surface type "<<value<<"\n";
+     eglGetConfigAttrib(display,*config,EGL_TRANSPARENT_TYPE,&value);
+     std::cout<<"--------------------------------------------------------------------------\n";
+  }
+
   bool init() {
     auto display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
     eglInitialize(display, 0, 0);
 
+    if (getenv("SHOW_CONFIGS"))
+    {
+        EGLBoolean result;
+        EGLint numConfigs;
+        
+        // first we call getConfigs with a NULL to see how many configs we have
+        result=eglGetConfigs(display,NULL,0,&numConfigs);
+        std::cout<< "number of configs found "<<numConfigs<<"\n";
+        // now we create a buffer to store all our configs
+        EGLConfig *configs = new EGLConfig[numConfigs];
+        // and copy them into our buffer (don't forget to delete once done)
+        result=eglGetConfigs(display,configs,numConfigs,&numConfigs);
+
+        std::cout<<"--------------------------------------------------------------------------\n";
+        for(int i=0; i<numConfigs; ++i)
+        {
+            std::cout<<"Config #"<<i<<"\n";
+            dumpConfig(display, &configs[i]);
+        }
+        // now clear up the configs now we have done
+        delete [] configs;
+    }
+    
     const EGLint attribs[] = {EGL_RENDERABLE_TYPE,
-                              EGL_OPENGL_ES_BIT,
+                              EGL_OPENGL_ES3_BIT_KHR | EGL_OPENGL_ES2_BIT | EGL_OPENGL_ES_BIT,
                               EGL_BLUE_SIZE,
                               8,
                               EGL_GREEN_SIZE,
@@ -75,13 +156,29 @@ public:
       LOGE("Unable to retrieve EGL config");
       return false;
     }
+    else {
+        dumpConfig(display, &config);
+    }
 
-    const EGLint context_attribs[] = {EGL_CONTEXT_MAJOR_VERSION, 2,
-                                      EGL_CONTEXT_MINOR_VERSION, 0, EGL_NONE};
+    const EGLint surfaceAttributes[] = {
+            EGL_WIDTH, 16,
+            EGL_HEIGHT, 16,
+            EGL_NONE
+    };
+
+    // This is just a dummy surface that it needed to make an OpenGL context current (bind it to this thread)
+    EGLSurface dummySurfaceForPlugin = eglCreatePbufferSurface(display, config, surfaceAttributes);
+    if (dummySurfaceForPlugin == EGL_NO_SURFACE) {
+      LOGE("Unable to create pbuffer surface !");
+    }
+
+    const EGLint context_attribs[] = {EGL_CONTEXT_CLIENT_VERSION, 3,
+                                      EGL_NONE, EGL_NONE,
+                                      EGL_NONE};
 
     context_ = eglCreateContext(display, config, nullptr, context_attribs);
 
-    if (eglMakeCurrent(display, EGL_NO_SURFACE, EGL_NO_SURFACE, context_) ==
+    if (eglMakeCurrent(display, dummySurfaceForPlugin, dummySurfaceForPlugin, context_) ==
         EGL_FALSE) {
       LOGE("Unable to eglMakeCurrent");
       return false;
